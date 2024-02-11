7cd8ce5f345f0526e8100ad94dfeae979452aca1
karriv18 <krlclarence@gmail.com>
2024-02-11 21:39:17 +0800

    pages for active

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const activePage = window.location.pathname;
-const navlist = document.querySelectorAll("li")
+const navlist = document.querySelectorAll("li");
 const navlink = document.querySelectorAll("a");
 
-navlist.forEach(link => {
-  navlink.forEach(item => {
-    if(item.href.includes(`${activePage}`)){
-      // link.classList.add("active")
-      item.parentNode.classList.add("active")
+navlist.forEach((link) => {
+  navlink.forEach((item) => {
+    if (item.href.includes(`${activePage}`)) {
+      item.parentNode.classList.add("active");
     }
-
-  })
-})
-
+  });
+});
